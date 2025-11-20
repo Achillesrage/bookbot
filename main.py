8cf27fefc764c46b3e8bb6446c0f77e5a7cf5add
Achillesrage <bud1683@gmail.com>
2025-11-20 15:42:14 -0500

    added word count

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(path_to_file):
 		file_contents = f.read()
 	return file_contents
 
+def num_of_words(b):
+	words = b.split()
+	return len(words)
+		
+
 def main():
 	book = get_book_text("books/frankenstein.txt")
-	print(book)
+	num_words = num_of_words(book)
+	print(f"Found {num_words} total words")
 
 main()
